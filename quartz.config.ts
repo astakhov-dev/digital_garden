9e527ee6598c6d3dbb299ae02de655307a6365ae
astakhov-dev <astakhov.o@gmail.com>
2026-08-31 23:11:46 +0500

    fix: update baseUrl to custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "astakhov-dev.github.io/digital_garden",
+    baseUrl: "olegbatalov.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
