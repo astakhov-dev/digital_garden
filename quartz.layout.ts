4aa915bc41f329caa84a22d056c51c80bbc76ec7
astakhov-dev <astakhov.o@gmail.com>
2026-09-20 18:52:25 +0000

    fix: восстановить транслитерацию, редиректы и брендинг после повторного сбоя автобэкапа

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,12 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.Subscribe()],
+  afterBody: [Component.Subscribe(), Component.SidebarAutoHide()],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Telegram: "https://t.me/delavporyadke",
+      LinkedIn: "https://www.linkedin.com/in/olegbatalov/",
+      Instagram: "https://www.instagram.com/olegbatalovv/",
     },
   }),
 }
@@ -23,6 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
+    Component.StatusBadge(),
     Component.TagList(),
   ],
   left: [
@@ -41,7 +43,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -52,3 +52,5 @@ export {
   ConditionalRender,
   Subscribe,
 }
+export { default as StatusBadge } from "./StatusBadge"
+export { default as SidebarAutoHide } from "./SidebarAutoHide"
